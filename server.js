ad4964e7efaa3ebc0416191bd69d198413351c2a
yazan <yaz.sneneh@gmail.com>
2021-03-07 11:48:01 +0200

    edits

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 'use strict';
+
 // .......................................................................... IMPORTS
 const express = require('express');
 const cors = require('cors');
@@ -6,28 +7,35 @@ const pg = require('pg');
 require('dotenv').config();
 const override = require('method-override');
 const superAgent = require('superagent');
+
 // ........................................................................... CONFIGURATIONS
 const app = express();
-app.use(cors())
-
-app.use(express.static('public'));
-app.set('view engine', 'ejs');
+app.use(cors());
 
 app.use(override('_method'));
+app.use(express.urlencoded({ extended: true }));
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
 
 const PORT = process.env.PORT;
 const client = new pg.Client(process.env.DATABASE_URL);
 // const client = new pg.Client({
 //     connectionString: process.env.DATABASE_URL,
 //     ssl: { rejectUnauthorized: false }
-// });
-// .............................................................................. ROUTES
-app.get('/', handleHomePage)
+// })
 
-// ............................................................................... FUNCTIONS
+// ................................................................................. ROUTES
+app.get('/', handleHomePage);
+app.get('/searches/songs', handleSongsearch)
+// ............................................................................... HANDLERS
 function handleHomePage(req, res) {
     res.render('index');
 }
+
+// ............................................................................... FUNCTIONS
+
+
 // .............................................................................. CONNECTION
 
 client.connect()
