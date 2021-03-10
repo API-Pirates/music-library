9ebf9f9add6302d68bd236eb7a2817ff004724d5
yazan <yaz.sneneh@gmail.com>
2021-03-10 11:47:40 +0200

    heroku db client

--- server.js
+++ server.js
@@ -16,12 +16,12 @@ app.use(override('_method'));
 app.use(express.urlencoded({ extended: true }));
 
 app.set('view engine', 'ejs');
-app.use(express.static('public'))
+app.use(express.static('public'));
 
 const PORT = process.env.PORT;
-const client = new pg.Client(process.env.DATABASE_URL);
+/*  const client = new pg.Client(process.env.DATABASE_URL); */
 
-// const client = new pg.Client({ connectionString: process.env.DATABASE_URL, ssl: { rejectUnauthorized: false } })
+const client = new pg.Client({ connectionString: process.env.DATABASE_URL, ssl: { rejectUnauthorized: false } })
 
 // .............................................................................. ROUTES
 
